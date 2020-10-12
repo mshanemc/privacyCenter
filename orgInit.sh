e812b4af01641dab15bbc4e447b9139968f0b6b7
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-12 13:51:16 -0500

    snapshot to get portability policy

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
+# removed because it's already in the snapshot
+# sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
 sfdx force:source:push
 sfdx force:org:open
