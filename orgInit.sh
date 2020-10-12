a60c5f3b2b91dd862e81d9483344edd289262fe1
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-12 10:17:11 -0500

    plus package

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
 sfdx force:source:push
 sfdx force:org:open
