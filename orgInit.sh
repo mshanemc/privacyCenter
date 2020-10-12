59e260006a13b06ab01f93e346f2ec7eb8753c3d
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-12 14:04:32 -0500

    password because we'll need for API?

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,5 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 # removed because it's already in the snapshot
 # sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
 sfdx force:source:push
+sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open
