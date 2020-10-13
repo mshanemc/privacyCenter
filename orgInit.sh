ce2b842768363ef0fe7bfaea208611b3742af426
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-13 17:06:51 -0500

    some sample data

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
 # removed because it's already in the snapshot
 # sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
 sfdx force:source:push
-sfdx shane:user:password:set -l User -g User -p sfdx1234
+# sfdx automig:load -d data
+# sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open
