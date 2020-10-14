a5660acc6933a07141ee88a210ed6e740d6928dc
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-14 10:29:07 -0500

    snapshot and non-snapshot versions

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
-# removed because it's already in the snapshot
-# sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
+sfdx force:org:create -f config/project-scratch-def-snapshot.json -d 1 -s -w 60
+# sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
+
+sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
 sfdx force:source:push
-# sfdx automig:load -d data
-# sfdx shane:user:password:set -l User -g User -p sfdx1234
+sfdx automig:load -d data
+sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open
