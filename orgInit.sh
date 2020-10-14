185224d58250730a8a399e276be60449184b4210
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-14 12:18:12 -0500

    favorite the individual

--- orgInit.sh
+++ orgInit.sh
@@ -6,3 +6,4 @@ sfdx force:source:push
 sfdx automig:load -d data
 sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open -p /lightning/n/privacycenter__Home
+sfdx shane:data:favorite -o Individual -w "FirstName = 'Shane' and LastName = 'McLaughlin'" -l "Shane McLaughlin"
