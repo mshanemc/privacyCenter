e74d646c5fe12bcda9bbb32ff610abc0b646bf65
mshanemc <shane.mclaughlin@salesforce.com>
2020-10-26 16:47:01 -0500

    new package version

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,9 @@
 sfdx force:org:create -f config/project-scratch-def-snapshot.json -d 1 -s -w 60
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -w 60
 
-sfdx force:package:install --package 04t3t000002s8kRAAQ -w 20 --noprompt
+sfdx force:package:install --package 04t3t000002s8wj -w 20 --noprompt
 sfdx force:source:push
-sfdx automig:load -d data
+sfdx automig:load -d data --concise
 sfdx shane:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open -p /lightning/n/privacycenter__Home
 sfdx shane:data:favorite -o Individual -w "FirstName = 'Shane' and LastName = 'McLaughlin'" -l "Shane McLaughlin"
